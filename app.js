ab6f5d56d5d26a733b267700120b9d94e065efb8
Hector Alvarez <87918859+HectorA97@users.noreply.github.com>
2021-10-09 17:21:36 -0300

    falta arreglar overlay

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
-var btnAbrirpopup = document.getElementById('pa'),
-    overlay = document.getElementById('overlay'),
-    popup = document.getElementById('popup'),
-    btnCerrarpopup = document.getElementById('btn-cerrar');
+var btnAbrirpopup = document.getElementById('pa');
+var overlay = document.getElementById('overlay');
+var popup = document.getElementById('popup');
+var btnCerrarpopup = document.getElementById('btn-cerrar');
 
 btnAbrirpopup.addEventListener('click', function(){
     overlay.classList.add('active');
+    popup.classList.add('active');
+});
+
+btnCerrarpopup.addEventListener('click', function(){
+    overlay.classList.remove('active');
+    popup.classList.remove('active');
 });
