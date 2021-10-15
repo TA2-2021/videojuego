6f45aa7f98a2b5930eefa2f0dc1454916e415eba
Neekomata <onahuel2002@gmail.com>
2021-10-15 20:09:28 -0300

    Comenté todo lo que hice para asi tener claro las funciones de cada cosa

--- php/login.php
+++ php/login.php
@@ -19,33 +19,48 @@
 
         if($this->verificarUsuario($nombreUsuario)){
             try {
+                //crear objeto de base de datos PDO
                 $base = new PDO('mysql:host = localhost; dbname untitlev1', 'root', '');
                 $base-> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-                    
+                    //Sentencia
                     $sql = 'INSERT INTO usuario VALUE (0, :nombre, :clave, 0, 0)';
+
+                    //Preparar objeto RESULTADO para absorver el resultado de la sentencia
                     $resultado = $base->prepare($sql);
+
+                    //Ejecutar sentencia
+                    //!Los "marcadores" :nombre y :clave funcionan como parametros dentro de la sentencia sql, nos referimos a ellos con ':nombre_marcador'
+                    //!Seguido de => para dirigir a que valor estamos refiriendo
                     $resultado->execute(array(':nombre' => $nombreUsuario, ':clave' => $claveUsuario));
 
+                    //Cerrar sentencia
                     $resultado->closeCursor();
                 
             } catch (Exception $e) {
-                echo 'Error al registrar ... <br> Error: '. $e->getMessage();
+                //emitir alerta en caso de error
+                echo '<script lenguaje="javascript">alert("Error...:'. $e->getMessage() .'");</script>';
             }//TryCatch
         }else
             echo 'Usuario ya existente...';
         }//Funcion Registrar
         
         public function verificarUsuario ($nombreU){
+            //crear objeto de base de datos PDO
             $base = new PDO('mysql:host = localhost; dbname untitlev1', 'root', '');
             $base-> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
+            //Sentencia
             $sql = 'SELECT nombre FROM usuario WHERE nombre = ?';
+            //Preparar onjeto RESULTADO para emitir el resultado de la sentencia
             $resultado = $base-> prepare($sql);
+            //la funcion Execute retorna BOOLEANO, por ende, si existe sera false, y sino true y que registre el usuario
             if($resultado-> execute(Array($nombreU))){
                 return false;
             }else
                 return true;
         }
+
+
     
     }//Clase Login
 ?>
